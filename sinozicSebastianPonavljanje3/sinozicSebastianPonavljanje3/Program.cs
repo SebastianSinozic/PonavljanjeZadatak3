f3d25c805f4ccda0083267a386a297a301e16167
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2020-11-12 11:00:40 +0100

    Add files via upload

---
+++ sinozicSebastianPonavljanje3/sinozicSebastianPonavljanje3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinozicSebastianPonavljanje3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite 1. stringa ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Upisite 2. stringa ");
+            string b = Console.ReadLine();
+            Console.WriteLine("Upisite 3. stringa ");
+            string c = Console.ReadLine();
+
+
+            string x = a + b + c;
+            x = new string(x.Reverse().ToArray());
+
+
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
